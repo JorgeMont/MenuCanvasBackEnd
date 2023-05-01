e98567ac95ae1777db956db81232489a2c9bd678
jorgemont <monterrosaspix@gmail.com>
2023-05-01 14:27:16 -0600

    Agregando cliente y modificando modelos

--- Models/Menu.js
+++ Models/Menu.js
@@ -1,9 +1,12 @@
 const mongoose = require ('mongoose');
 const eschema = mongoose.Schema;
 
+const Platillo = require('./Platillo ');
+
 const schemaMenu = new eschema({
     nombre: {type: 'String', required: true},
-    categorias: {type: 'Array', required: true}
+    categorias: {type: 'Array', required: true},
+    platillos: {type: [Platillo], required: false}
 });
 
 module.exports = mongoose.model('Menu', schemaMenu);
