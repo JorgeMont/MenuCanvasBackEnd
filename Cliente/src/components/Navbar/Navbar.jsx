c685eb8ae7ca5f30eab226fdd46a5f4edb6d3367
jorgemont <monterrosaspix@gmail.com>
2023-05-12 19:13:53 -0600

    Mejora a navbar, enlazando rutas, auth0

--- Cliente/src/components/Navbar/Navbar.jsx
+++ Cliente/src/components/Navbar/Navbar.jsx
@@ -1,52 +1,62 @@
-import React from 'react'
-import {AiOutlineSetting} from "react-icons/ai";
-import {BiExit} from "react-icons/bi"
+import React from "react";
+import { Link } from "react-router-dom";
+import LoginButton from "../LoginBtn/LoginBtn";
+import canvasLogo from "../../Assets/Images/menucanvas_logo.png";
+import userIcon from '../../Assets/Images/user_icon.png';
+// import {AiOutlineSetting} from "react-icons/ai";
+// import {BiExit} from "react-icons/bi"
 
 function Navbar() {
   return (
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-  <a class="navbar-brand" href="/home">
-    <img src="https://picsum.photos/200"  alt="logo" />
-    MenuCanvas
-    </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarText">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/home">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/nosotros">Nosotros</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/planes">Planes</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/contacto">Contacto</a>
-        </li>
-      </ul>
-      <span class="navbar-text">
-        <a href="/profile">
-            <img src="https://picsum.photos/200" alt="profile-pic" />
-            <div class="btn-group dropdown">
-                <button type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                    Nombre
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/home">
+          <img src={canvasLogo} alt="logo" />
+          MenuCanvas
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarText"
+          aria-controls="navbarText"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarText">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link to={"/"}> Home</Link>
+            </li>
+            <li className="nav-item">
+            <Link to={"/Nosotros"}> Nosotros</Link>
+            </li>
+            <li className="nav-item">
+              <Link to={"/planes"}> Planes</Link>
+            </li>
+            <li className="nav-item">
+              <Link to={"/Contacto"}> Contacto</Link>
+            </li>
+          </ul>
+          <span className="navbar-text">
+            <img src={userIcon} alt="profile-pic" />
+            {/* <div className="btn-group dropdown">
+                <button type="button" className="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                    Log In
                 </button>
-                <ul class="dropdown-menu">
+                <ul className="dropdown-menu">
                     <li><a href="/">Ajustes de la cuenta <i><AiOutlineSetting/></i></a></li>
                     <li><a href="/">Cerrar session <i><BiExit/></i> </a></li>
                 </ul>
-            </div>
-        </a>
-      </span>
-    </div>
-  </div>
-</nav>
-
+            </div> */}
+            <LoginButton />
+          </span>
+        </div>
+      </div>
+    </nav>
   );
-};
+}
 
-export default Navbar
+export default Navbar;
--- Cliente/src/index.js
+++ Cliente/src/index.js
@@ -1,13 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Auth0Provider } from "@auth0/auth0-react";
+import "./index.css";
+import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <App />
+    <Auth0Provider
+      domain="dev-cmc62k64vrx62xke.us.auth0.com"
+      clientId="A4sm2PcEQOehSexs5F8L64QLwMohCZtp"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <App />
+    </Auth0Provider>
   </BrowserRouter>
 );
-
