6a989fe568898d571e0665f00bae80d76810fcbc
AldoMardel <aldomardel@gmail.com>
2023-05-14 20:16:27 -0600

    Actualizacion Rama Integración

--- Cliente/src/App.js
+++ Cliente/src/App.js
@@ -4,20 +4,21 @@ import './App.scss';
 import CreateProfilePage from './Pages/CreateProfilePage';
 import MenuEditorPage from './Pages/MenuEditorPage';
 import SelectYourPlan from './Pages/SelectYourPlan';
+import LandingPage from './Pages/LandingPage/LandingPage';
 function App() {
   return (
   <>
     <Routes>
-    <Route path='/' element = {<p>home</p>} />
+    <Route path='/' element = {<LandingPage />} />
     <Route path='/planes' element = {<SelectYourPlan />} />
-    <Route path='/login' element = {<p>login </p>} />
+    <Route path='/login' element = {<p>login</p>} />
     <Route path='/signup' element = {<p>signup</p>} />
     <Route path='/payment' element = {<p>payment</p>} />
     <Route path='/purchase' element = {<p>purchase</p>} />
     <Route path='/dashboard' element = {<p>dashboard</p>} />
     <Route path='/edit/:idplatillo' element = {<p>edit</p>} />
     <Route path='/my_qr' element = {<p>my_qr</p>} />
-    <Route path='/create-menu' element = {<MenuEditorPage />} />
+    <Route path='/create_menu' element = {<MenuEditorPage />} />
     <Route path='/suscription' element = {<p>suscription</p>} />
     <Route path='/profile' element = {<CreateProfilePage />} />
     </Routes>
